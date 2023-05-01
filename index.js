cc6276b3a5ae7ea00719113a9af9bb601211d444
bijaykumarpun <bijay.workplace@gmail.com>
2023-05-02 05:41:28 +0545

    feat: use workflow from the same repo

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try{
+const name = core.getInput('who-to-greet');
+console.log('Hello ${name}');
+
+const time = (new Date()).toTimeString();
+core.setOutput("time",time);
+} catch (error){
+core.setFailed(error.message);
+}
