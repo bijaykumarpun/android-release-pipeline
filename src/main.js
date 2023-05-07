0550c6f64230bd87b299dbf87c63427c22107e3f
bijaykumarpun <bijay.workplace@gmail.com>
2023-05-07 05:46:54 +0545

    chore: cleanups

--- src/main.js
+++ src/main.js
@@ -2,19 +2,13 @@ import * as publisherApi from '@googleapis/androidpublisher';
 import { promises as fs, createReadStream } from 'fs';
 
 const core = require('@actions/core');
-// const fs = require('fs').promises;
 const androidPublisher = publisherApi.androidpublisher('v3');
 
 try {
 
-const serviceAccountJson = core.getInput('serviceAccountJson');
-const packageName = core.getInput('packageName');
-const releaseFileDir = core.getInput('releaseFileDir');
-//const track = core.getInput('track');
-//const mappingFileDir = core.getInput('mappingFileDir');
-    // console.log("Log test"+ serviceAccountJson);
-    // console.log("Log test" + packageName);
-    // console.log("Log test" + releaseFileDir);
+    const serviceAccountJson = core.getInput('serviceAccountJson');
+    const packageName = core.getInput('packageName');
+    const releaseFileDir = core.getInput('releaseFileDir');
 
     const serviceAccountFile = "serviceAccountJson.json";
     await fs.writeFile(serviceAccountFile, serviceAccountJson, function (err) {
@@ -41,8 +35,6 @@ const releaseFileDir = core.getInput('releaseFileDir');
         }
     );
 
-
-
 } catch (error){
-core.setFailed(error.message);
+    core.setFailed(error.message);
 }
