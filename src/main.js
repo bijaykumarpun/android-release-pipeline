0227d9506fd1940bd62050fdb159a393429f3e25
Bijay Kumar Pun <bijay.workplace@gmail.com>
2023-05-08 05:41:28 +0545

    feat: Setup production release pipeline (#2)
    feat: production release track

--- src/main.js
+++ src/main.js
@@ -2,25 +2,26 @@ import * as publisherApi from '@googleapis/androidpublisher';
 import { promises as fs, createReadStream } from 'fs';
 
 const core = require('@actions/core');
-// const fs = require('fs').promises;
 const androidPublisher = publisherApi.androidpublisher('v3');
+
+async function init() {
 
 try {
 
-const serviceAccountJson = core.getInput('serviceAccountJson');
-const packageName = core.getInput('packageName');
-const releaseFileDir = core.getInput('releaseFileDir');
-//const track = core.getInput('track');
-//const mappingFileDir = core.getInput('mappingFileDir');
-    // console.log("Log test"+ serviceAccountJson);
-    // console.log("Log test" + packageName);
-    // console.log("Log test" + releaseFileDir);
-
+    //Base setup
+    const serviceAccountJson = core.getInput('serviceAccountJson');
+    const packageName = core.getInput('packageName');
+    const releaseFileDir = core.getInput('releaseFileDir');
+    const releaseTrack = core.getInput('track');
+    const mappingFileDir = core.getInput('mappingFileDir');
     const serviceAccountFile = "serviceAccountJson.json";
+
     await fs.writeFile(serviceAccountFile, serviceAccountJson, function (err) {
         if (err) {
+            core.info('Error writing service account credential');
             console.log('Error');
         } else {
+            core.info('Successfully written service account credential');
             console.log('Successfully written');
         }
     });
@@ -30,7 +31,25 @@ const releaseFileDir = core.getInput('releaseFileDir');
         scopes: ['https://www.googleapis.com/auth/androidpublisher']
     });
 
-    androidPublisher.internalappsharingartifacts.uploadapk(
+    switch (releaseTrack) {
+        // case ReleaseTrack.INTERNAL_SHARING:
+        case 'internalShring':
+            uploadToInternalSharing(auth, packageName, releaseFileDir);
+
+        // case ReleaseTrack.PRODUCTION:
+        case 'production':
+            uploadToProduction(auth, packageName, '', releaseFileDir, mappingFileDir);
+
+    }
+
+} catch (error) {
+    core.setFailed(error.message);
+}
+
+}
+
+async function uploadToInternalSharing(auth, packageName, releaseFileDir) {
+    const uploadResult = await androidPublisher.internalappsharingartifacts.uploadapk(
         {
             auth: auth,
             packageName: packageName,
@@ -40,9 +59,115 @@ const releaseFileDir = core.getInput('releaseFileDir');
             }
         }
     );
+    const downloadUrl = uploadResult.data.downloadUrl;
 
-
-
-} catch (error){
-core.setFailed(error.message);
+    console.log(`DATA:\nUpload to internal shring\nURL: ${downloadUrl}`);
+    core.info(`DATA:\nUpload to internal shring\nURL: ${downloadUrl}`);
 }
+
+async function uploadToProduction(auth, packageName, releaseName, releaseFileDir, mappingFileDir) {
+    var versionCode = null;
+
+
+    //Create an Edit
+    const editResult = await androidPublisher.edits.insert({
+        auth: auth,
+        packageName: packageName
+    });
+    core.info(`Edit Id ${editResult.data.id}`);
+    console.log(`Edit Id ${editResult.data.id}`);
+
+    //Upload release files
+    if (releaseFileDir.endsWith('.apk')) {
+        const res = await androidPublisher.edits.apks.upload({
+            auth: auth,
+            packageName: packageName,
+            editId: editResult.data.id,
+            media: {
+                mimeType: 'application/vnd.android.package-archive',
+                body: createReadStream(releaseFileDir)
+            }
+        });
+        versionCode = res.data.versionCode;
+        console.log(`Version Code ${versionCode}`);
+        core.info(`Version Code ${versionCode}`)
+
+    } else if (releaseFileDir.endsWith('.aab')) {
+        const res = await androidPublisher.edits.bundles.upload({
+            auth: auth,
+            packageName: packageName,
+            editId: editResult.data.id,
+            media: {
+                mimeType: 'application/octet-stream',
+                body: createReadStream(releaseFileDir)
+
+            }
+        });
+        versionCode = res.data.versionCode;
+        console.log(`Version Code ${versionCode}`);
+        core.info(`Version Code ${versionCode}`)
+
+    } else Error('invalid release file');
+
+    //upload mapping file
+    const fileUploadResult = await androidPublisher.edits.deobfuscationfiles.upload({
+        auth: auth,
+        packageName: packageName,
+        editId: editResult.data.id,
+        apkVersionCode: versionCode,
+        deobfuscationFileType: 'proguard',
+        media: {
+            mimeType: 'application/octet-stream',
+            body: createReadStream(mappingFileDir)
+        }
+    });
+
+
+    //add releases to track
+    if (versionCode != null) {
+
+        await androidPublisher.edits.tracks.update(
+            {
+                auth: auth,
+                editId: editResult.data.id,
+                packageName: packageName,
+                track: 'production',
+
+                requestBody: {
+                    track: 'production',
+                    releases:
+
+                    {
+                        name: releaseName,
+                        // userFraction: status == 'completed' ? undefined : userFraction,
+                        status: 'completed',
+                        inAppUpdatePriority: 5,
+                        releaseNotes: [{
+                            language: 'en-US',
+                            text: ''
+                        }],
+                        versionCodes: [versionCode.toString()]
+                    }
+                }
+
+            }
+        )
+    } else throw Error('version code is null');
+
+    //finally commit
+    const commitResult = await androidPublisher.edits.commit({
+        auth: auth,
+        editId: editResult.data.id,
+        packageName: packageName,
+        // changesNotSentForReview: true
+    })
+
+}
+
+
+init();
+
+
+
+
+
---
+++ src/release_tracks.js
@@ -0,0 +1,10 @@
+// const ReleaseTrack = {
+//     INTERNAL_SHARING: 'internalSharing',
+//     ALPHA: 'alpha',
+//     BETA: 'beta',
+//     QA: 'qa',
+//     PRODUCTION: 'production'
+
+// }
+
+// exports.ReleaseTrack = ReleaseTrack;
